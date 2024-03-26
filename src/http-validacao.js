b7e57412b5bcb96175a914186a571000b58ff8f3
aluno Felipe <felipemesquita734@gmail.com>
2024-03-26 14:59:29 -0300

    Felipe

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,8 +1,14 @@
+import chalk from "chalk"
+
 async function checaStatus(listaURLs){
     const arrStatus= await Promise.all(
             listaURLs.map(async(url)=>{
-            const response = await fetch(url);
-            return response.status;
+            try{
+                const response = await fetch(url);
+                return response.status;
+            }catch(erro){
+               return manejaErros(erro);
+            }
         })
     )
     return arrStatus;
@@ -15,6 +21,15 @@ function extraiLinks(arrLinks){
 }
 
 
+function manejaErros(erro){
+    //console.log(chalk.red("algo deu errado"))
+    if (erro.cause.code==="ENOTFOUND"){
+        return "link não encontrado"
+    }else{
+        return "ocorreu um erro"
+    }
+}
+
 
 //criar uma função listaValidada
 export default async function listaValidada(listaDeLinks){
@@ -22,7 +37,10 @@ export default async function listaValidada(listaDeLinks){
     const links =extraiLinks(listaDeLinks);
     const status =await checaStatus(links);
     //console.log(status);
-    return status;
+    return listaDeLinks.map((objeto,indice)=>({
+        ...objeto,
+        status:status[indice]
+    }));
 }
 
 //[gatinho salsicha](http://gatinhosalsicha.com.br/)
